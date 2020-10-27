5ab3b8a377f769c65e454bd264a43d0d20352e66
st0dgy <72568988+st0dgy@users.noreply.github.com>
2020-10-27 19:48:41 +0200

    Add files via upload

---
+++ lab4.2/lab4.2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, A, B, y;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << "   |"
+		<< setw(7) << "y" << "    |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		A = 13.5 - 2 * x;
+		if (x <= -1)
+			B = exp(0.4 + x);
+		if (x > -1 && x < 1)
+			B = 1 - sin(x) * sin(x);
+		if (x >= 1)
+			B = cos(x) / (1 + sin(x) * sin(x));
+		y = A - B;
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
